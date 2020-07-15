58cdeb662a9e8cb2db2565b9a5c23b2ada4df740
Jacob <jacob.m.mcfaul@gmail.com>
2020-07-15 13:09:37 -0700

    Learned template styles/classes/active classes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,19 +4,18 @@ import { Component } from '@angular/core';
 @Component({
     selector: "courses",
     template: `
-    <h2>{{ title }}</h2>
-    <ul>
-        <li *ngFor="let course of courses">
-            {{ course }}
-        </li>
-    </ul>
+    <button (click)="activate()" [style.backgroundColor]="isActive ? 'black' : 'green'" class="btn btn-primary" [class.active]="isActive">Click here</button>
     `
 })
 export class CoursesComponent {
-    title = "List of courses";
-    courses;
+    // title = "List of courses";
+    // courses;
 
-    constructor(service: CoursesService) {
-        this.courses = service.getCourses()
+    // constructor(service: CoursesService) {
+    //     this.courses = service.getCourses()
+    // }
+    isActive = false
+    activate() {
+        this.isActive = !this.isActive
     }
 }

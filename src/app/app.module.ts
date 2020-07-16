25ca77882bbcbe8bbae092a93095ab53f081de39
Jacob <jacob.m.mcfaul@gmail.com>
2020-07-16 14:34:05 -0700

    Learned pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { AuthorsService } from './authors.service';
-import { CoursesService } from './courses.service';
-import { CoursesComponent } from './courses.component';
+import { CoursesService } from './courses/courses.service';
+import { CoursesComponent } from './courses/courses.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { InputComponent } from './input/input.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    FavoriteComponent,
+    InputComponent,
+    TitleCasePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     CoursesService,

a1d83defbc0a46e9e4c630216b4ef76d6b9ed705
Jacob <jacob.m.mcfaul@gmail.com>
2020-07-17 10:51:10 -0700

    Built titleCase function

--- src/app/title-case.pipe.ts
+++ src/app/title-case.pipe.ts
@@ -5,25 +5,32 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TitleCasePipe implements PipeTransform {
 
-  transform(value: String): any {
+  transform(value: string): any {
     if (!value) return null;
-
-    let small = [
-      'the', "of"
-    ]
 
     let words = value.split(' ');
     for (let i = 0; i < words.length; i++) {
-      if (i == 0) {
+      if (i == 0)
         words[i] = words[i].substr(0, 1).toUpperCase() + words[i].substr(1).toLowerCase();
-      }
-      else if (small.includes(words[i])) {
-        words[i].toLowerCase();
-      } else {
-        words[i] = words[i].substr(0, 1).toUpperCase() + words[i].substr(1).toLowerCase();
-      }
+
+      if (i > 0 && this.prepositions(words[i]))
+        words[i].toLowerCase()
+      else
+        words[i] = this.toTitleCase(words[i])
+
     }
     return words.join(' ')
+  }
+
+  private prepositions(word: string): boolean {
+    let small = [
+      'the', "of"
+    ]
+    return small.includes(word.toLowerCase());
+  }
+
+  private toTitleCase(word: string): string {
+    return word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
   }
 
 }
